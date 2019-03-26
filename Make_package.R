aa00505cee8be2fcb86a93b309d83775a7ae52c3
Dominik Liebl <liebl.dominik@gmail.com>
2019-03-26 14:35:27 +0100

    Simple Update

--- Make_package.R
+++ Make_package.R
@@ -1,4 +1,4 @@
-remove.packages("fdapoi")
+utils::remove.packages("fdapoi")
 devtools::document("fdapoi_pkg") 
 devtools::check("fdapoi_pkg")
 devtools::install("fdapoi_pkg")
@@ -6,9 +6,3 @@ devtools::install("fdapoi_pkg")
 
 # library("fdapoi")
 # help(package = "fdapoi")
-
-# BM.SIM(0,1,10)
-# OU.SIM(0,1,10,3)
-# 
-# ?FUN_PoI_BIC
-# ?is.error
